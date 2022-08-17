acc84adc2b8aca3bb4bd8304b53cb4213f78a6c6
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-08-17 11:41:21 -0300

    ajuste unidades

---
+++ includes/request_headers.py
@@ -0,0 +1,23 @@
+import requests
+
+def set_header(url, data):
+
+    hed = {
+        'authority': 'app.tecnofit.com.br',
+        'method': 'POST',
+        'path': '/relatorio/clienteAtivo/listar',
+        'scheme': 'https',
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-length': '18',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'cookie': '_ga=GA1.3.827632101.1650381763; _hjSessionUser_1292435=eyJpZCI6IjQ2OWVmYWJmLThkYjItNTNjYy1iNDZkLTliMWUyODFkZjNkMyIsImNyZWF0ZWQiOjE2NTAzODE3NjM0NDcsImV4aXN0aW5nIjp0cnVlfQ==; NPS_3add8e44_last_seen=1657211754841; _fw_crm_v=86780c54-0d51-4ec6-9fb0-6aba33bfa3ca; AWSALB=3H6V3/GsSL/fTf8P1hx0GDiC4wfc86DmdOBbnUHNdBU/qG1lgpzIkmTnbt0LuGbrfcqns8bhJhjyoq15RszIw9EM9j9RYgriJZLQL7FHE15eHo0wlWza5b+FHWGl; AWSALBCORS=3H6V3/GsSL/fTf8P1hx0GDiC4wfc86DmdOBbnUHNdBU/qG1lgpzIkmTnbt0LuGbrfcqns8bhJhjyoq15RszIw9EM9j9RYgriJZLQL7FHE15eHo0wlWza5b+FHWGl; _gid=GA1.3.535785240.1660677756; PHPSESSID=b8rrqg7ftdvapag74ovs5k1efa; _hjIncludedInSessionSample=0; _hjSession_1292435=eyJpZCI6ImYwNGJhZDZlLTYxZjAtNDJlOC04MDI5LThlMzc0Y2EyNDBiNyIsImNyZWF0ZWQiOjE2NjA3NDM2NzQ4NzMsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=0; NPS_3add8e44_throttle=1660786880500; _gat=1; _gat_UA-52493754-1=1',
+        'newrelic': 'eyJ2IjpbMCwxXSwiZCI6eyJ0eSI6IkJyb3dzZXIiLCJhYyI6Ijg2NDMzNyIsImFwIjoiNDMxNDg2MjM2IiwiaWQiOiJhZjIwZTg2NDEzZTU3MTlmIiwidHIiOiI0ZDc1MDUyMjU3YjM3ZTA0ODdkYjYyODZjMWU0NzVlNyIsInRpIjoxNjYwMzE5Nzg2MTI1fX0=',
+        'origin': 'https://app.tecnofit.com.br',
+        'referer': 'https://app.tecnofit.com.br/relatorio/clienteAtivo'
+    }
+
+    resultado = requests.post(url, json=data, headers=hed)
+
+    return resultado
--- tecnofit_conversao_clientes.py
+++ tecnofit_conversao_clientes.py
@@ -1,22 +1,9 @@
 import pandas as pd
 import json
 import xlsxwriter
-from headers.request_headers import set_header
-from headers.request_headers_auth import set_header_auth
-
-array_unidades = [
-    ['58D7B066A6BAD7C3F84CBF88770F5CE05E1BFC07A8ED6F6A574FD63A0D86FF32', '4003', 'Cristo'],
-    ['8617597dceea7278ed31b7fda9adbc13ec068f85f4539f8c112bbd966e75f686', '4117', 'CROSSFIT TRES FIGUEIRAS'],
-    ['2a86c127c068a8f82867de890129d9270a693dfe9d5211f216e05d524b2069df', '10840', 'Canoas'],
-    ['f951c7049159fe39ba8eafb25dd0155a198dee12a1a7cfe909a30256ea7d826a', '11344', 'Petrópolis'],
-    ['b4bfe708ecfe0c1211bafca5bc96e9b768690e5169f83cc826323b67be76f40b', '11345', 'Menino Deus'],
-    ['d9ee0a7b4516ae5730e68a86ca6d6b3c0e788814991ccb0bff3aeeab028031f5', '13803', 'Santa Cruz do Sul'],
-    ['8c1e2d71365777ab6527e6963f6dcdff167c726eb103639ee427f2dbdc15e691', '13883', 'BOM FIM'],
-    ['37867682f37d4d7b90f52d74e21e918dbcdb73a197aa1689fbb7564750df80c0', '18726', 'Crossfit Cachoeira'],
-    ['d332101203e4bec2ceeb52b0c579e4b2e885a69bddf67ebf325cc5ae40f4f1f8', '26340', 'CACHOEIRINHA'],
-    ['5BE855829B6256A819FA0A5AA74091C4CEF71F0C8F40AB220106DAADE3A80DF4', '33259', 'Caxias'],
-    ['07f43a2870ada97df99c667a1e7734ceb4e33c22f1ed34424e745b404d069894', '56711', 'Guaiba'],
-]
+from includes.request_headers import set_header
+from includes.request_headers_auth import set_header_auth
+from includes.unidades import array_unidades
 
 workbook = xlsxwriter.Workbook('Tecnofit_Conversao_de_Clientes.xlsx')
 worksheet = workbook.add_worksheet()
@@ -46,7 +33,7 @@ for unidade in array_unidades:
 
     data = {
         'data_inicial' : '01/01/2015',
-        'data_final' : '31/07/2022',
+        'data_final' : '31/08/2022',
     }
 
     url = 'https://app.tecnofit.com.br/relatorio/novasMatriculas/listar'
