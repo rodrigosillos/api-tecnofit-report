c3ef84986338230003c4aa704664fc4c1419634b
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-09-30 12:57:06 -0300

    cookie

--- includes/request_headers.py
+++ includes/request_headers.py
@@ -12,7 +12,7 @@ def set_header(url, data):
         'accept-language': 'en-US,en;q=0.9',
         'content-length': '18',
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'cookie': '_ga=GA1.3.827632101.1650381763; _hjSessionUser_1292435=eyJpZCI6IjQ2OWVmYWJmLThkYjItNTNjYy1iNDZkLTliMWUyODFkZjNkMyIsImNyZWF0ZWQiOjE2NTAzODE3NjM0NDcsImV4aXN0aW5nIjp0cnVlfQ==; NPS_3add8e44_last_seen=1657211754841; _fw_crm_v=86780c54-0d51-4ec6-9fb0-6aba33bfa3ca; _gid=GA1.3.1823908122.1663109767; _hjIncludedInSessionSample=0; _hjSession_1292435=eyJpZCI6IjliOGY3YjMxLTQ2NjctNDJjOS1hYWFjLTJiMWEwNjI2MDc3MyIsImNyZWF0ZWQiOjE2NjMxMDk3Njg1NjcsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=0; PHPSESSID=ic0ade3m21ula5o21fajch3tjs; _gat=1; _gat_UA-52493754-1=1',
+        'cookie': '_ga=GA1.3.1012669217.1664550735; _gid=GA1.3.928097642.1664550735; _hjFirstSeen=1; _hjSession_1292435=eyJpZCI6ImYzNTI1NGM5LWUwM2QtNDBjZi05OWI1LTAyMDI4NjMwNDEzZiIsImNyZWF0ZWQiOjE2NjQ1NTA3MzYwMzUsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=0; NPS_3add8e44_last_seen=1664551585731; NPS_3add8e44_throttle=1664594786162; _hjSessionUser_1292435=eyJpZCI6IjVlMTYwM2FkLTZlOWEtNTk1Yy05ZDk1LTQ2YWViZjQ5MjZkOSIsImNyZWF0ZWQiOjE2NjQ1NTA3MzUyNDksImV4aXN0aW5nIjp0cnVlfQ==; _fw_crm_v=107114ff-c99a-4b4b-8595-0db5b4b95a95; _hjIncludedInSessionSample=0; PHPSESSID=o68kdud9tkgl4na91aflsa59jl; _gat=1; _gat_UA-52493754-1=1',
         'newrelic': 'eyJ2IjpbMCwxXSwiZCI6eyJ0eSI6IkJyb3dzZXIiLCJhYyI6Ijg2NDMzNyIsImFwIjoiNDMxNDg2MjM2IiwiaWQiOiJhZjIwZTg2NDEzZTU3MTlmIiwidHIiOiI0ZDc1MDUyMjU3YjM3ZTA0ODdkYjYyODZjMWU0NzVlNyIsInRpIjoxNjYwMzE5Nzg2MTI1fX0=',
         'origin': 'https://app.tecnofit.com.br',
         'referer': 'https://app.tecnofit.com.br/relatorio/clienteAtivo'
